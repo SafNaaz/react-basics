4d8ceb59b2addd7f134d2500b19d51dd31ade21a
SAFNAS <iamsafnas@gmail.com>
2020-06-03 00:24:32 +0530

    simple react component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-alert("it wooooooooooorks");
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+class TodoComponent extends React.Component{
+  render(){
+    return (
+      <h1>Hellooo</h1>
+    );
+  }
+}
+
+//put component into html page
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
