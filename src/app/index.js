34e65989faf6b9b059bed35f0a73240a79e35cc4
SAFNAS <iamsafnas@gmail.com>
2020-06-03 11:24:54 +0530

    all about props

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,30 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-class TodoComponent extends React.Component{
-  render(){
+class TodoComponent extends React.Component {
+  render() {
     return (
-      <h1>Hellooo</h1>
+      <div>
+        <h1>Hellooo</h1>
+        <p>Hellooo</p>
+        <p>{this.props.msg}</p>
+        <p>
+          <strong>Cheese Name:</strong>
+          {this.props.cheese.name}
+        </p>
+      </div>
     );
   }
 }
 
+var myCheese = {
+  name: "Camembert",
+  smellFactor: "Extreme Pong",
+  price: "3.50",
+};
+
 //put component into html page
-ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+ReactDOM.render(
+  <TodoComponent msg="I like cheese" cheese={myCheese} />,
+  document.getElementById("todo-wrapper")
+);
