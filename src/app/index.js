c3c325e3e56c874f0bf4cd5da13630f031b5c753
SAFNAS <iamsafnas@gmail.com>
2020-06-03 11:38:44 +0530

    all about state

--- src/app/index.js
+++ src/app/index.js
@@ -2,29 +2,26 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 class TodoComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: ["wash up", "eat some cheese", "take a nap"],
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hellooo</h1>
-        <p>Hellooo</p>
-        <p>{this.props.msg}</p>
-        <p>
-          <strong>Cheese Name:</strong>
-          {this.props.cheese.name}
-        </p>
+      <div id="todo-list">
+        <p>The busiest people have the most leisure</p>
+        <ul>
+          <li>{this.state.todos[0]}</li>
+          <li>{this.state.todos[1]}</li>
+          <li>{this.state.todos[2]}</li>
+        </ul>
       </div>
     );
   }
 }
 
-var myCheese = {
-  name: "Camembert",
-  smellFactor: "Extreme Pong",
-  price: "3.50",
-};
-
 //put component into html page
-ReactDOM.render(
-  <TodoComponent msg="I like cheese" cheese={myCheese} />,
-  document.getElementById("todo-wrapper")
-);
+ReactDOM.render(<TodoComponent />, document.getElementById("todo-wrapper"));
