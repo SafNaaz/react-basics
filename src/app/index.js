1413609b2c2fcf9c0adf14585cc3e4ff2706e7c6
SAFNAS <iamsafnas@gmail.com>
2020-06-03 12:15:03 +0530

    handle events

--- src/app/index.js
+++ src/app/index.js
@@ -7,41 +7,55 @@ class TodoComponent extends React.Component {
     this.state = {
       todos: ["wash up", "eat some cheese", "take a nap", "buy flowers"],
     };
+    this.onDelete = this.onDelete.bind(this);
   }
 
   render() {
     var todos = this.state.todos;
     todos = todos.map((item, index) => {
-      return <TodoItem item={item} key={index} />;
+      return <TodoItem item={item} key={index} onDelete={this.onDelete} />;
     });
     return (
       <div id="todo-list">
-        <p onClick={this.clicked}>
-          The busiest people have the most leisure...
-        </p>
+        <p>The busiest people have the most leisure...</p>
         <ul>{todos}</ul>
       </div>
     );
   }
 
-  //custom functions
-
-  clicked() {
-    console.log("you clicked me");
+  onDelete(item) {
+    var updatedTodos = this.state.todos.filter((val, index) => {
+      return item !== val;
+    });
+    this.setState({
+      todos: updatedTodos,
+    });
   }
 }
 
 //create TodoItem component
 
 class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
   render() {
     return (
       <li>
         <div className="todo-item">
           <span className="item-name">{this.props.item}</span>
+          <span className="item-delete" onClick={this.handleDelete}>
+            -x
+          </span>
         </div>
       </li>
     );
+  }
+
+  handleDelete() {
+    this.props.onDelete(this.props.item);
   }
 }
 
