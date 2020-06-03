f7ae2c942e1f56e07c0a4767afb41fe76eecef53
SAFNAS <iamsafnas@gmail.com>
2020-06-03 11:48:23 +0530

    cycle through data

--- src/app/index.js
+++ src/app/index.js
@@ -5,26 +5,19 @@ class TodoComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: ["wash up", "eat some cheese", "take a nap"],
-      age: 30,
+      todos: ["wash up", "eat some cheese", "take a nap", "buy flowers"],
     };
   }
 
   render() {
-    var ager = setTimeout(() => {
-      this.setState({
-        age: 35,
-      });
-    }, 5000);
+    var todos = this.state.todos;
+    todos = todos.map((item, index) => {
+      return <li>{item}</li>;
+    });
     return (
       <div id="todo-list">
-        <p>The busiest people have the most leisure</p>
-        <p>{this.state.age}</p>
-        <ul>
-          <li>{this.state.todos[0]}</li>
-          <li>{this.state.todos[1]}</li>
-          <li>{this.state.todos[2]}</li>
-        </ul>
+        <p>The busiest people have the most leisure...</p>
+        <ul>{todos}</ul>
       </div>
     );
   }
